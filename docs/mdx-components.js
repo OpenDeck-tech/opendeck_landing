95203971e89a2823b786b85951097c840dfa45d6
Tobi John <tobioluwafemi58@gmail.com>
2026-01-24 09:07:43 +0000

    updates docs

---
+++ docs/mdx-components.js
@@ -0,0 +1,19 @@
+import { CodeBlock } from './components/CodeBlock'
+import { APIEndpoint } from './components/APIEndpoint'
+import { Card } from './components/Card'
+import { Alert } from './components/Alert'
+
+export const mdxComponents = {
+  pre: (props) => <CodeBlock {...props} />,
+  code: (props) => {
+    // If it's a code block (has className), use CodeBlock
+    if (props.className) {
+      return <CodeBlock {...props} />
+    }
+    // Otherwise, it's inline code
+    return <code className="bg-matte-dark text-white px-2 py-1 rounded text-sm font-mono border border-matte-border" {...props} />
+  },
+  APIEndpoint,
+  Card,
+  Alert,
+}
