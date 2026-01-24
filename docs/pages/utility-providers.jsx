93f8d4a771fc71073d02670167e8ddb6caf32b0e
Tobi John <tobioluwafemi58@gmail.com>
2026-01-24 09:07:49 +0000

    updates docs

--- docs/pages/utility-providers.jsx
+++ docs/pages/utility-providers.jsx
@@ -1,28 +1,12 @@
 import Layout from '../components/Layout'
 import { MDXProvider } from '@mdx-js/react'
-import { CodeBlock } from '../components/CodeBlock'
-import { APIEndpoint } from '../components/APIEndpoint'
-import { Card } from '../components/Card'
-import { Alert } from '../components/Alert'
+import { mdxComponents } from '../mdx-components'
 import UtilityProviders from '../content/utility-providers.mdx'
-
-const components = {
-  pre: (props) => <CodeBlock {...props} />,
-  code: (props) => {
-    if (props.className) {
-      return <CodeBlock {...props} />
-    }
-    return <code className="bg-matte-dark text-white px-2 py-1 rounded text-sm font-mono border border-matte-border" {...props} />
-  },
-  APIEndpoint,
-  Card,
-  Alert,
-}
 
 export default function UtilityProvidersPage() {
   return (
     <Layout>
-      <MDXProvider components={components}>
+      <MDXProvider components={mdxComponents}>
         <div className="mdx-content">
           <UtilityProviders />
         </div>
