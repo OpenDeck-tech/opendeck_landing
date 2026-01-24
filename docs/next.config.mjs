620b086f5784b86a310776d3a3ff426f3b397ddf
Tobi John <tobioluwafemi58@gmail.com>
2026-01-24 08:59:55 +0000

    Adds mjs

---
+++ docs/next.config.mjs
@@ -0,0 +1,25 @@
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeHighlight,
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+    ],
+  },
+})
+
+export default withMDX(nextConfig)
